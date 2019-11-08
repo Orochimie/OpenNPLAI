af2843a868a196bf63607d1d2427b6fa35b67f33
Orochimie <medkihal@hotmail.fr>
2019-11-08 15:54:46 +0100

    AI TEST

---
+++ src/main/java/com/OpenNLPAI/OpenNLPAI/OpenNlpaiApplication.java
@@ -0,0 +1,13 @@
+package com.OpenNLPAI.OpenNLPAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenNlpaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenNlpaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OpenNLPAI/OpenNLPAI/OpenNlpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenNLPAI.OpenNLPAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenNlpaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
